cc5c6de0624fa62a10044ee8acfe9d0ef4bf16ba
mathia <mnoble@malachiarts.com>
2017-10-17 22:06:22 +0000

    Improve model with training data processing (filter, augment and crop)

--- model.py
+++ model.py
@@ -1,46 +1,61 @@
 #!/usr/bin/env python
 
-# Basic LeNet architecture in Keras for training with the Behavioral Clone project data
-# from https://classroom.udacity.com/nanodegrees/nd013/parts/fbf77062-5703-404e-b60c-95b78b2f3f9e/modules/6df7ae49-c61c-4bb2-a23e-6527e69209ec/lessons/46a70500-493e-4057-a78e-b3075933709d/concepts/db4960f9-c8d3-4baf-9c7e-678341c1cf3e
-
 import csv
 import cv2
 import numpy as np
 
-## Load the training data from the CSV file into Numpy arrays
-lines = []
-with open('data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
+def load_data():
+    ## Load the training data from the CSV file into Numpy arrays
+    lines = []
+    with open('data/driving_log.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            # Discarding all data points where the steering angle is 0.
+            # There are alot of examples in the supplied training data set where it is 0 and this biases the model.
+            if float(line[3]) != 0:
+                lines.append(line)
+    
+    images = []
+    measurements = []
+    for line in lines:
+        try:
+            filename = line[0].split('/')[-1]
+            current_path = 'data/IMG/' + filename
+            image = cv2.cvtColor(cv2.imread(current_path), cv2.COLOR_BGR2RGB)
+            images.append(image)
+            measurement = float(line[3])
+            measurements.append(measurement)
+            # Augmenting the data set by adding horizontally flipped images with labeled with inverted steering angles.
+            # This evens out the number of left and right steering data points in the data set.
+            images.append(cv2.flip(image,1))
+            measurements.append(-measurement)
+        except Exception as err:
+            print("Couldn't process the following line.")
+            print(line)
+            print('Skipping it.')
+        
+    print('Loaded {} images with {} steering angle measurements.'.format(len(images), len(measurements)))
+    return np.array(images), np.array(measurements)
 
-images = []
-measurements = []
-for line in lines:
-    filename = line[0]
-    current_path = 'data/' + filename
-    image = cv2.imread(current_path)
-    images.append(image)
-    measurement = float(line[3])
-    measurements.append(measurement)
-
-X_train = np.array(images)
-y_train = np.array(measurements)
-
-print(len(images))
-print(len(measurements))
+X_train, y_train = load_data()
 
 
 ## Load Keras
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
+# Building a sequential Keras model as LeNet is a linear stack of layers.
+# https://keras.io/getting-started/sequential-model-guide/
 model = Sequential()
 
-# Perform data normalization, zero mean and unit variance assuming image data values with normal distibution from 0-255.
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+# Crop the images.  Just need the model to focus on the image of the road, the rest is extraneous.
+# Removes 55 lines from the top to get rid of things above the horizon and 25 line from the bottom to remove the car's hood.  Nothing removed from left or right sides.
+model.add(Cropping2D(cropping=((55, 25), (0, 0)), input_shape=(160, 320, 3)))
+
+# Perform data normalization, zero mean and unit variance assuming the images' pixel values have a normal distibution from 0-255.
+model.add(Lambda(lambda x: x / 255.0 - 0.5))
 
 # Build a classic LeNet model
 model.add(Convolution2D(6,5,5,activation="relu"))
@@ -52,7 +67,10 @@ model.add(Dense(120))
 model.add(Dense(84))
 model.add(Dense(1))
 
+# Using the Adam optimizer with a Mean Square Error loss function.
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
+# Found performance did not improve with more than 6 epochs.
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=6)
 
-model.save('model-lenet.h5')
+# Save the model architecture and its learned weights so it can "drive" the simulated car with drive.py
+model.save('model.h5')
